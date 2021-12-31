55ff5fbdf3564f316138fa8e4553ff0c5a563999
Mohit kumar <60257288+m0hit-kumar@users.noreply.github.com>
2021-12-31 12:34:20 +0530

    education tab edited

--- js/switchPages.js
+++ js/switchPages.js
@@ -16,66 +16,66 @@ function openTab(tabtype, tabName) {
   }
 }
 
-
-
-
-
-
 /* Store the element in el */
-let el = document.getElementsByClassName('project')
+let el = document.getElementsByClassName("edu-card");
 
 /* Get the height and width of the element */
-const height = el.clientHeight
-const width = el.clientWidth
+const height = el.clientHeight;
+const width = el.clientWidth;
 
 /*
-  * Add a listener for mousemove event
-  * Which will trigger function 'handleMove'
-  * On mousemove
-  */
-el.addEventListener('mousemove', handleMove)
+ * Add a listener for mousemove event
+ * Which will trigger function 'handleMove'
+ * On mousemove
+ */
+el.addEventListener("mousemove", handleMove);
 
 /* Define function a */
 function handleMove(e) {
   /*
-    * Get position of mouse cursor
-    * With respect to the element
-    * On mouseover
-    */
+   * Get position of mouse cursor
+   * With respect to the element
+   * On mouseover
+   */
   /* Store the x position */
-  const xVal = e.layerX
+  const xVal = e.layerX;
   /* Store the y position */
-  const yVal = e.layerY
-  
+  const yVal = e.layerY;
+
   /*
-    * Calculate rotation valuee along the Y-axis
-    * Here the multiplier 20 is to
-    * Control the rotation
-    * You can change the value and see the results
-    */
-  const yRotation = 20 * ((xVal - width / 2) / width)
-  
+   * Calculate rotation valuee along the Y-axis
+   * Here the multiplier 20 is to
+   * Control the rotation
+   * You can change the value and see the results
+   */
+  const yRotation = 20 * ((xVal - width / 2) / width);
+
   /* Calculate the rotation along the X-axis */
-  const xRotation = -20 * ((yVal - height / 2) / height)
-  
+  const xRotation = -20 * ((yVal - height / 2) / height);
+
   /* Generate string for CSS transform property */
-  const string = 'perspective(500px) scale(1.1) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)'
-  
+  const string =
+    "perspective(500px) scale(1.1) rotateX(" +
+    xRotation +
+    "deg) rotateY(" +
+    yRotation +
+    "deg)";
+
   /* Apply the calculated transformation */
-  el.style.transform = string
+  el.style.transform = string;
 }
 
 /* Add listener for mouseout event, remove the rotation */
-el.addEventListener('mouseout', function() {
-  el.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
-})
+el.addEventListener("mouseout", function () {
+  el.style.transform = "perspective(500px) scale(1) rotateX(0) rotateY(0)";
+});
 
 /* Add listener for mousedown event, to simulate click */
-el.addEventListener('mousedown', function() {
-  el.style.transform = 'perspective(500px) scale(0.9) rotateX(0) rotateY(0)'
-})
+el.addEventListener("mousedown", function () {
+  el.style.transform = "perspective(500px) scale(0.9) rotateX(0) rotateY(0)";
+});
 
 /* Add listener for mouseup, simulate release of mouse click */
-el.addEventListener('mouseup', function() {
-  el.style.transform = 'perspective(500px) scale(1.1) rotateX(0) rotateY(0)'
-})
+el.addEventListener("mouseup", function () {
+  el.style.transform = "perspective(500px) scale(1.1) rotateX(0) rotateY(0)";
+});
